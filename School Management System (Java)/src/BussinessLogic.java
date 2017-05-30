ebcaafc2ff76f34ffa020f2d2d2e16b56c6b2042
Yasir <raza142010@gmail.com>
2017-05-30 18:15:41 +0500

    School Management System
    School Management System

---
+++ School Management System (Java)/src/BussinessLogic.java
@@ -0,0 +1,41 @@
+
+public class BussinessLogic 
+{
+   
+    
+    public int ITCP(int ITCPSessional, int ITCPMid, int ITCPFinal)
+    {
+        return ITCPSessional+ITCPMid+ITCPFinal;
+    }
+    
+    public int CP(int CPSessional, int CPMid, int CPFinal)
+    {
+        return CPSessional+CPMid+CPFinal;
+    }
+    
+    public int OOP(int OOPSessional, int OOPMid, int OOPFinal)
+    {
+        return OOPSessional+OOPMid+OOPFinal;
+    }
+    
+    public int VP(int VPSessional, int VPMid, int VPFinal)
+    {
+        return VPSessional+VPMid+VPFinal;
+    }
+    
+    public int FP(int FPSessional, int FPMid, int FPinal)
+    {
+        return FPSessional+FPMid+FPinal;
+    }
+    
+    public int TotalResult(int ResultITCPL, int ResultCPL, int ResultOOPL, int ResultVPL, int ResultIFPL)
+    {
+        return ResultITCPL+ResultCPL+ResultOOPL+ResultVPL+ResultIFPL;
+        
+    }
+    
+    
+    
+
+    
+}
